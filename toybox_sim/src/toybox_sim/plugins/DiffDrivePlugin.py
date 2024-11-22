e06f14904d89dfd9a8ab6410b63e161c2033765c
Wes Fletcher <wfletcher@geisel-software.com>
2024-11-22 09:13:09 -0500

    refactor to use the Velocity primitive

--- toybox_sim/src/toybox_sim/plugins/DiffDrivePlugin.py
+++ toybox_sim/src/toybox_sim/plugins/DiffDrivePlugin.py
@@ -8,7 +8,8 @@ import toybox_core as tbx
 
 from toybox_sim.plugins.plugins import Plugin, BaseControlPluginIF, PLUGIN_TYPE
 from toybox_sim.entities import Entity
-from toybox_sim.primitives import Pose
+from toybox_sim.primitives import Pose, Velocity
+from toybox_sim.world import World
 
 from toybox_msgs.core.Test_pb2 import TestMessage
 from toybox_msgs.state.Velocity_pb2 import Velocity as VelocityMsg
@@ -28,16 +29,18 @@ class DiffDrivePlugin(Plugin, BaseControlPluginIF):
         Plugin.__init__(self, id=id)
         BaseControlPluginIF.__init__(self)
         
-        #### default values ####
         self._owner: Entity
         self._id: str = id
+        
         # wheel constants
         self._wheel_base = 0.1
         self._wheel_radius = 0.05
+        
         # velocity parameters
-        self._vel_target: Tuple[float, float] = (0.0,0.0)
+        self._vel_target: Velocity = Velocity()
         self._vel_target_timeout: float = 1.0
         self._use_target_timeout: bool = True
+
         # timing
         self._time_last_call: float = -1.0
         self._time_last_target: float = -1.0
@@ -142,17 +145,14 @@ class DiffDrivePlugin(Plugin, BaseControlPluginIF):
         if self._use_target_timeout:
             if (call_time - self._time_last_target) >= self._vel_target_timeout:
                 print(f'Time since last velocity target update exceeds limit. Setting velocity to (0.0,0.0)')
-                self.set_target_velocity(0.0, 0.0)
+                self.set_target_velocity(Velocity())
 
         self._time_last_call = call_time
-
         self._vel_pub.publish(VelocityMsg(x=0.1, y=1.1, z=2.2))
-
 
     def set_target_velocity(
         self,
-        x_vel: float,
-        theta_vel: float,
+        target_vel: Velocity,
         timeout: bool = True
     ) -> None:
         """
@@ -162,7 +162,7 @@ class DiffDrivePlugin(Plugin, BaseControlPluginIF):
             x_vel (float): _description_
             theta_vel (float): _description_
         """
-        self._vel_target = (x_vel, theta_vel)
+        self._vel_target = target_vel
         self._use_target_timeout = timeout
         self._time_last_target = time.time()
 
@@ -171,8 +171,7 @@ class DiffDrivePlugin(Plugin, BaseControlPluginIF):
 
     def calc_wheel_vels(
         self,
-        x_vel: float,
-        theta_vel: float
+        vel: Velocity,
     ) -> Tuple[float, float]:
         """
         Decomposes a desired target velocity [x, theta] into L- and R-wheel velocities
@@ -181,14 +180,14 @@ class DiffDrivePlugin(Plugin, BaseControlPluginIF):
         R: float = self._wheel_radius
         W: float = self._wheel_base
 
-        v_l: float = ((2.0 * x_vel) - (theta_vel * W)) / (2.0 * R)
-        v_r: float = ((2.0 * x_vel) + (theta_vel * W)) / (2.0 * R)
+        v_l: float = ((2.0 * vel.linear.x) - (vel.angular.z * W)) / (2.0 * R)
+        v_r: float = ((2.0 * vel.linear.x) + (vel.angular.z * W)) / (2.0 * R)
 
         return (v_l, v_r)
 
     def get_pose_change(
         self, 
-        velocity: Tuple[float, float],
+        velocity: Velocity,
         current_pose: Pose,
         dt: float
     ) -> Tuple[float, float, float]:
@@ -203,11 +202,9 @@ class DiffDrivePlugin(Plugin, BaseControlPluginIF):
             Tuple[float, float, float]: _description_
         """
         # desired velocity
-        v_x: float = velocity[0]
-        v_theta: float = velocity[1]
+        v_x: float = velocity.linear.x
+        v_theta: float = velocity.angular.z
         # current state
-        x: float = current_pose.position.x
-        y: float = current_pose.position.y
         theta: float = current_pose.orientation.theta
 
         # calculates velocity deltas
