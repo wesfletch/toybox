4802669dff5a13331997d6528174e98ceebfaff4
Wes Fletcher <wfletcher@geisel-software.com>
2024-12-06 12:56:53 -0500

    checkpoint: before trying to fix nodes

--- toybox_core/src/toybox_core/scripts/launch.py
+++ toybox_core/src/toybox_core/scripts/launch.py
@@ -2,22 +2,22 @@
 
 from dataclasses import dataclass
 from importlib.metadata import entry_points, EntryPoints
-from inspect import signature, Signature
-from typing import Any, Dict, Generic, List, TypeAlias, TypeVar
+from inspect import signature, Signature, Parameter
+from typing import Any, Dict, Generic, Iterable, List, TypeVar, get_args, Union
 
-from toybox_core.Launch import launch, Launchable
+from toybox_core.Launch import launch, Launchable, LaunchError
 
 TBX_NAMESPACE: str = "tbx."
 TBX_NODES: str = TBX_NAMESPACE + "nodes"
 
-# PARAM: TypeAlias = tuple[str, type]
 
 T = TypeVar('T')
 @dataclass
-class Param(Generic[T]):
+class NodeParam(Generic[T]):
     name: str
     type: type
-    value: T | None = None
+    value: T | Parameter.empty = Parameter.empty
+    required: bool = True
 
 
 def discover_all_launchable_nodes() -> Dict[str,Launchable]:
@@ -39,9 +39,9 @@ def discover_all_launchable_nodes() -> Dict[str,Launchable]:
 
     return all_launchable_nodes
 
-def get_launchable_node_params(package_name: str, node_name: str) -> List[Param]:
+def get_launchable_node_params(package_name: str, node_name: str) -> Dict[str,NodeParam]:
 
-    returned: List[Param] = []
+    returned: Dict[str,NodeParam] = {}
 
     full_node_name: str = f"{package_name}.{node_name}"
     
@@ -55,36 +55,74 @@ def get_launchable_node_params(package_name: str, node_name: str) -> List[Param]
             if param.name == "self":
                 continue
 
-            returned.append(Param(name=param.name, type=param.annotation))
+            returned[param.name] = NodeParam(
+                name=param.name, 
+                type=param.annotation,
+                required=(param.default is Parameter.empty),
+                value=param.default)
+        
+        # TODO: This would be a good place for "declared" non-constructor params,
+        # like from a @classmethod or something....
 
     return returned
 
-def param_list_to_mapping(params: List[Param]) -> Dict[str,Any]:
+def validate_params(params: Dict[str,NodeParam]) -> bool:
 
-    returned: Dict = {}
+    for name, param in params.items():
+        if param.required and param.value is Parameter.empty:
+            print(f"Failed to provide value for required param <'{param.name}'> with type <'{param.type}'>")
+            return False
+        elif not isinstance(param.value, param.type):
+            print(f"Provided value for param <'{param.name}'> does not match type <'{param.type}'>: {param.value}")
+            return False
+        
+    return True
 
-    for param in params:
-        returned[param.name] = param.type(param.value)
+def unravel_params(params: Dict[str,NodeParam]) -> Dict[str,Any]:
+
+    returned: Dict[str,Any] = {}
+
+    # Attempt to cast each param value to the correct type
+    for name, param in params.items():
+        
+        value: Any | Parameter.empty = Parameter.empty
+
+        # Handling unions: if the type is not a Union, union will be None
+        union: tuple[Any,...] = get_args(param.type)
+        if not union:
+            # The type isn't a Union. Attempt to cast directly to the type.
+            try:
+                value = param.type(param.value)
+            except (TypeError,ValueError):
+                print(f"Failed to cast value <'{param.value}'> to type <'{param.type}'>")
+        else:
+            # This type is a Union, try to cast to each member of the Union until one of them works.
+            for type_in_union in union:
+                try:
+                    value = type_in_union(param.value)
+                    break
+                except (TypeError,ValueError):
+                    continue
+
+        if value is not Parameter.empty:
+            returned[param.name] = value
+        else:
+            print(f"Failed to cast value <'{param.value}'> to type <'{param.type}'>")
 
     return returned
 
 def main() -> None:
 
-    # from toybox_examples.src.toybox_examples.pico_bridge import PicoBridge
-    # pico_bridge: PicoBridge = PicoBridge(name="pico_bridge")
-
-    # launch(to_launch=pico_bridge)
     nodes = discover_all_launchable_nodes()
-    params: List[Param] = get_launchable_node_params(package_name="toybox_examples", node_name="Listener")
+    params: Dict[str,NodeParam] = get_launchable_node_params(package_name="toybox_examples", node_name="Listener")
+    params["name"].value = "test"
+    print(params)
+    
+    if not validate_params(params):
+        return
 
-    for param in params:
-        if param.name == "name":
-            param.value = "test"
-
-    listener = nodes["toybox_examples.Listener"](**param_list_to_mapping(params))
+    listener = nodes["toybox_examples.Listener"](**unravel_params(params))
     launch(to_launch=listener)
-
-
 
 if __name__ == "__main__":
     main()
