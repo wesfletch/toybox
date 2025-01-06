cac54fa24bd39d2145c4f3316ca1aad3d910d200
Wes Fletcher <wesfletch@gmail.com>
2025-01-06 05:45:10 -0500

    fully implements the launch functionality using toybox metadata; allows for discovery of package information using info; makes server.py a launchable node; adds `resource` metadata tag for things that should be discoverable but aren't launch files or message files

--- toybox_core/src/toybox_core/scripts/info.py
+++ toybox_core/src/toybox_core/scripts/info.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 from typing import List
 
 from toybox_core.RegisterServer import Client, get_registered_clients_rpc
+from toybox_core.metadata import find_tbx_packages, ToyboxMetadata
 
 
 def list_clients() -> None:
@@ -12,6 +14,25 @@ def list_clients() -> None:
     for client in clients:
         print(str(client))
 
+def list_packages() -> None:
+
+    all_pkgs: dict[str,ToyboxMetadata] = find_tbx_packages()
+    for package in all_pkgs.values():
+        print(f"{package.package_name}: {package.package_root}")
+
+
+def info_about_package(package_name: str) -> None:
+
+    all_pkgs: dict[str,ToyboxMetadata] = find_tbx_packages()
+    meta: ToyboxMetadata | None = all_pkgs.get(package_name, None)
+
+    if meta is None:
+        print(f"Couldn't find info about package <{package_name}>")
+        return
+
+    print(meta.human_readable())
+
+
 def main() -> None:
 
     assert len(sys.argv) >= 1, "Needs at least 1 argument"
@@ -19,9 +40,18 @@ def main() -> None:
     
     if verb == "clients":
         list_clients()
+    if verb == "package":
+        package_name: str | None = None
+        try:
+            package_name = sys.argv[2]
+            info_about_package(package_name=package_name)
+        except IndexError:
+            list_packages()
     else:
         print("What?")
         sys.exit(1)
+    
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
